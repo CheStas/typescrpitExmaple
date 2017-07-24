336e87e4ba8dca76e0b756356344a1c8618b2c1c
Stas <chstas2@gmail.com>
2017-07-24 15:01:15 +0300

    fix enemy type in improvedFighter

--- src/public/battle/improvedFighter.ts
+++ src/public/battle/improvedFighter.ts
@@ -1,7 +1,7 @@
-import {Fighter} from './fighter'
+import {Fighter, IFighter} from './fighter'
 
 export class ImprovedFighter extends Fighter {
-    doubleHit(enemy: Fighter, point: number) {
+    doubleHit(enemy: IFighter, point: number) {
         return super.hit(enemy, point*2)
     }
 }
